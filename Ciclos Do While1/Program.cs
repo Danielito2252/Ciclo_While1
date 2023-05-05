b2946ba751ba257a25392c00b1a1331924b37215
Danielito2252 <hbarriosb4@miumg.edu.gt>
2023-05-05 17:20:03 -0600

    No se cambio nada

---
+++ Ciclos Do While1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Manejo de ciclos - While - do While
+//Crear un programa para sumar numeros aleatoriamente
+//Variable
+
+
+int suma = 0;
+int randomNumber = 0;
+//instanciamos rnd
+Random rnd = new Random();
+//Ciclo DoWhile: Ejecutate mientras esto no ocurra
+do
+{
+    randomNumber = rnd.Next(1, 5);
+    Console.WriteLine($"Numero Aleatorio generado: {randomNumber}");
+    suma += randomNumber;
+} while (suma <= 100);
+Console.WriteLine($"La suma de los numeros aleatorios es: {suma}");
